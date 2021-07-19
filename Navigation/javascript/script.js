193da4a4e6e0041e788d4257f575b65e6e5abf6a
Joey-Haas <jhaas.github@gmail.com>
2021-07-19 20:03:04 +0200

    Side navigation
    Side navigation will be available on screens that are max 769px wide.
    
    The sidenavigation has a accordion menu and both the container, and accordion are triggered by JavaScript.

--- Navigation/javascript/script.js
+++ Navigation/javascript/script.js
@@ -1,9 +1,36 @@
-// Search bar
+// Open burger menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Close burger menu
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// Reveal searchbar
 function openSearch() {
     // Opens to the full height of the navigation bar
-    document.getElementById("searchBar").style.height = "82px";
+    document.getElementById("searchBar").style.height = "70px";
   }
 
+// Hide searchbar
   function closeSearch() {
     document.getElementById("searchBar").style.height = "0";
   }
+
+//   Openen accordion menu in navigatie
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+} 
