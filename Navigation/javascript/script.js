4fb87c608c95e489593f422f55ec0c447c72c328
Joey-Haas <jhaas.github@gmail.com>
2021-07-19 19:50:06 +0200

    Search
    Clicking on the search text or icon will reveal a searchbar. Opening and closing is done through JavaScript.

---
+++ Navigation/javascript/script.js
@@ -0,0 +1,9 @@
+// Search bar
+function openSearch() {
+    // Opens to the full height of the navigation bar
+    document.getElementById("searchBar").style.height = "82px";
+  }
+
+  function closeSearch() {
+    document.getElementById("searchBar").style.height = "0";
+  }
